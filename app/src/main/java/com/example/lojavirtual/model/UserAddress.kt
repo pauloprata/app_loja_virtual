e0b968412662fb22d15493b10bfc7441aa72a07e
Paulo Prata <paulodanielprata@gmail.com>
2022-05-24 11:24:24 -0300

    criando model do userAdress

---
+++ app/src/main/java/com/example/lojavirtual/model/UserAddress.kt
@@ -0,0 +1,15 @@
+package com.example.lojavirtual.model
+
+import java.io.Serializable
+
+data class UserAddress(
+    val id: String,
+    val user: User,
+    val addressLine1: String,
+    val addressLine2: String,
+    val number: String,
+    val state: String,
+    val city: String,
+    val zipCode: String,
+    val country: String
+): Serializable
