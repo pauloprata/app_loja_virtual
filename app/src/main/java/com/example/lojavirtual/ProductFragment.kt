5670fdddd4303cd7c7e8dfba40d4ef17b6eed2a7
Paulo Prata <paulodanielprata@gmail.com>
2022-05-24 11:14:03 -0300

    Class ProductFragment

---
+++ app/src/main/java/com/example/lojavirtual/ProductFragment.kt
@@ -0,0 +1,39 @@
+package com.example.lojavirtual
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.lojavirtual.adapter.ProductAdapter
+import com.example.lojavirtual.adapter.ProductCategoryAdapter
+import com.example.lojavirtual.model.Product
+import com.example.lojavirtual.model.ProductCategory
+
+class ProductFragment : Fragment() {
+    lateinit var recycleAdapter: RecyclerView
+    lateinit var arrayProduct: List<Product>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(arguments != null){
+            arrayProduct = (arguments?.getSerializable("CATEGORY") as ProductCategory).products
+        }
+    }
+        override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?    ): View? {
+            val view: View = inflater.inflate(R.layout.fragment_product, container,false)
+            recycleAdapter = view.findViewById(R.id.rv_product)
+
+            val adapterProduct = ProductAdapter(arrayProduct, requireContext())
+
+            recycleAdapter.adapter = adapterProduct
+            recycleAdapter.layoutManager = GridLayoutManager(requireContext(), 2)
+
+            return view
+
+
+
+    }
+}
